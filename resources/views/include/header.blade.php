7fc5869d004d9847697e3a16f2580ba0dc3112e1
Alan0777 <camp.alan3@gmail.com>
2023-05-26 20:52:44 -0300

    Atualização

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -7,10 +7,10 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                </li>
                 @auth
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('logout') }}">Logout</a>
                     </li>
--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,15 @@ use App\Http\Controllers\AuthManager;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
 
-Route::get('/login', [AuthManager::class, 'login'])->name('login');
-Route::post('/login', [AuthManager::class, 'loginPOST'])->name('login.post');
+Route::get('/', [AuthManager::class, 'login'])->name('login');
+Route::post('/', [AuthManager::class, 'loginPOST'])->name('login.post');
 
 Route::get('/registration', [AuthManager::class, 'registration'])->name('registration');
 Route::post('/registration', [AuthManager::class, 'registrationPOST'])->name('registration.post');
+
+Route::get('/home', function () {
+    return view('welcome');
+})->name('home');
 
 Route::get('/logout', [AuthManager::class, 'logout'])->name('logout');
